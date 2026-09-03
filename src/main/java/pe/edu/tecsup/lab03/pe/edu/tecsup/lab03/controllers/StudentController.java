cfca73147dd81d7a476037168dbaaeb49f738c55
eduardo <quiquiasotoeduardo@gmail.com>
2026-09-03 16:50:00 -0500

    Clase actualizada por Piero Guevara

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+        public void saludar() {
+            System.out.println("Hola soy Piero Guevara");
+        }
 }
