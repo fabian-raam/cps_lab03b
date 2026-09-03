c7578f7a4ef3b1c736942bb98794672165103897
RamsesAlvrz <ramses.alvarez@tecsup.edu.pe>
2026-09-03 17:31:53 -0500

    Clase actualizada por Ramses Alvarez sprint -2

--- src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/pe/edu/tecsup/lab03/services/StudentService.java
@@ -2,6 +2,6 @@ package pe.edu.tecsup.lab03.pe.edu.tecsup.lab03.services;
 
 public class StudentService {
     public void saludar() {
-        System.out.println("Hola soy Ramses Alvarez");
+        System.out.println("Hola soy Ramses Alvarez de nuevo");
     }
 }
