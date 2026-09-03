9ef2858be35f867c384544218b0f656f7302c6a4
yojhanHuanca <yhuancayucra@gmail.com>
2026-09-03 17:12:15 -0500

    Creacion de la clase repositories.StudentRepository eneque

---
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.lab03.repositories;
+
+public class StudentRepository {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int resultado = a + b;
+        System.out.println("Resultado: " + resultado);
+    }
+}
